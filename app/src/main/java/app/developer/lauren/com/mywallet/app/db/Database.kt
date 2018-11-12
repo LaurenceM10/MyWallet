fbfa5cc23c31c36e75369ed43bfba4e5fb2b710f
LaurenceM10 <laurenmontenegro10@gmail.com>
2018-11-11 22:41:54 -0600

    Added basic room database structure

--- app/src/main/java/app/developer/lauren/com/mywallet/app/db/Database.kt
+++ app/src/main/java/app/developer/lauren/com/mywallet/app/db/Database.kt
@@ -1,7 +1,34 @@
 package app.developer.lauren.com.mywallet.app.db
 
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.Room
+import android.arch.persistence.room.RoomDatabase
+import android.content.Context
+import app.developer.lauren.com.mywallet.app.entities.User
+
 /**
- * Created by macyarin on 11/11/18.
- */
-class Database {
+* Created by Lauren Steven on 11/11/18.
+*/
+@Database(entities = [(User::class)], version = 1, exportSchema = false)
+abstract class Database : RoomDatabase() {
+    companion object {
+        private var INSTANCE: Database? = null
+
+        fun getInstance(context: Context): Database? {
+            if (INSTANCE == null) {
+                synchronized(Database::class) {
+                    INSTANCE = Room.databaseBuilder(context.applicationContext,
+                            Database::class.java, "Wallet")
+                            .allowMainThreadQueries()
+                            .build()
+                }
+            }
+
+            return INSTANCE
+        }
+
+        fun destroyInstance() {
+            INSTANCE = null
+        }
+    }
 }
