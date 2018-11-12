883f34d07996f5d553371cfa848ead3a74989850
LaurenceM10 <laurenmontenegro10@gmail.com>
2018-11-11 22:36:46 -0600

    Changed IDE template

--- app/src/main/java/app/developer/lauren/com/mywallet/app/entities/User.kt
+++ app/src/main/java/app/developer/lauren/com/mywallet/app/entities/User.kt
@@ -4,8 +4,8 @@ import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
 
 /**
- * Created by macyarin on 11/11/18.
- */
+* Created by Lauren Steven on 11/11/18.
+*/
 @Entity
 data class User(var username: String) {
     @PrimaryKey(autoGenerate = true) var id: Long = 0
